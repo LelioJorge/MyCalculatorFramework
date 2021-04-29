f5c9bcad7e507bb617a90565be51fff545a6f2f4
Lelio Jorge <lelio_ljj@hotmail.com>
2021-04-29 15:16:08 -0300

    Adicionar projeto framework

---
+++ MyCalculatorFramework/MyCalculatorFramework/Calculator.swift
@@ -0,0 +1,25 @@
+//
+//  Calculator.swift
+//  MyCalculatorFramework
+//
+//  Created by Lelio Jorge on 29/04/21.
+//
+
+import Foundation
+
+public class Calculator {
+    public init() {}
+    
+    public func sum(x: Float, y: Float) -> Float {
+        return x + y
+    }
+    public func subtract(x: Float, y: Float) -> Float {
+        return x - y
+    }
+    public func mutiply(x: Float, y: Float) -> Float {
+        return x * y
+    }
+    public func divide(x: Float, y: Float) -> Float {
+        return x / y
+    }
+}
---
+++ MyCalculatorFramework/MyCalculatorFramework/MyCalculatorFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyCalculatorFramework.h
+//  MyCalculatorFramework
+//
+//  Created by Lelio Jorge on 29/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyCalculatorFramework.
+FOUNDATION_EXPORT double MyCalculatorFrameworkVersionNumber;
+
+//! Project version string for MyCalculatorFramework.
+FOUNDATION_EXPORT const unsigned char MyCalculatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCalculatorFramework/PublicHeader.h>
+
+
